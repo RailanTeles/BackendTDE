4a3334347926a4471cd261eb36b6cceda422574b
Railan Teles <railan.oliveira.ofc@gmail.com>
2025-09-18 21:17:06 -0300

    Swagger adicionado

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from flask_cors import CORS
 from routes.usuario_routes import usuario_routes
+from flasgger import Swagger
 
 app = Flask(__name__)
 
 CORS(app)
+
+swagger = Swagger(app, template_file="swagger.yml")
 
 app.register_blueprint(usuario_routes)
 
