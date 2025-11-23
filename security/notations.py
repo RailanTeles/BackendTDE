6bd5a75060e2e961aa5a41976f9323829ce8377d
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-11-23 01:30:08 -0300

    Desfazendo as merdas de Guilherme

--- security/notations.py
+++ security/notations.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import request, jsonify, g
+from flask import request
 from utils.jwt_util import decode_token
 from dao.usuario_dao import UsuarioDao
 
@@ -11,25 +11,21 @@ def token_required(f):
 
         # Verifica se há token
         if not token:
-            return jsonify({"msg": "Não autorizado"}), 401
+            return {"msg": "Não autorizado"}, 401
 
         # Verifica se o formato é válido
         if not token.startswith('Bearer '):
-            return jsonify({"msg": "Formato de token inválido"}), 401
+            return {"msg": "Formato de token inválido"}, 401
 
         user_id = decode_token(token)
         if not user_id:
-            return jsonify({"msg": "Token inválido"}), 401
-
+            return {"msg": "Token inválido"}, 401
+        
         # Verifica se o usuário existe
         user = usuarioDao.obterUsuarioId(user_id)
+
         if not user:
-            return jsonify({"msg": "Token inválido"}), 404
-
-        # SALVA O USER_ID NO CONTEXTO GLOBAL
-        g.user_id = user_id
-        print(f"✅ User ID autenticado: {user_id}")
-
+            return {"msg": "Token inválido"}, 404
         return f(*args, **kwargs)
 
     return decorated
